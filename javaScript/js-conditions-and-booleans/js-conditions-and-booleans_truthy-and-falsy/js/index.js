e01f500eea51e304549e39e57d21b8d246c177a7
Daniel Riley <dann.riley@icloud.com>
2023-08-18 12:07:17 +0200

    initial commit comments

--- javaScript/js-conditions-and-booleans/js-conditions-and-booleans_truthy-and-falsy/js/index.js
+++ javaScript/js-conditions-and-booleans/js-conditions-and-booleans_truthy-and-falsy/js/index.js
@@ -1,2 +1,50 @@
 const truthyOrFalsy = Boolean(3 >= 2);
 console.log("The expression is " + truthyOrFalsy);
+
+// 2 == "2"
+//  truthy  falsy
+truthy;
+
+// 2 === "2"
+//  truthy  falsy
+falsy;
+
+// 2 === 2
+//  truthy  falsy
+truthy;
+
+// "hello world"
+//  truthy  falsy
+truthy;
+
+// ""
+//  truthy  falsy
+falsy;
+
+// true
+//  truthy  falsy
+truthy;
+
+// 0
+//  truthy  falsy
+falsy;
+
+// -50
+//  truthy  falsy
+truthy;
+
+// 50
+//  truthy  falsy
+truthy;
+
+// 5 > 1
+//  truthy  falsy
+truthy;
+
+// "1 > 10"
+//  truthy  falsy
+truthy;
+
+// "false"
+//  truthy  falsy
+truthy;
