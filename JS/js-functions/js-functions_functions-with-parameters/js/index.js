bfc2b488d8779fb8406c3447bf89478abc60d64f
Daniel Riley <dann.riley@icloud.com>
2023-08-21 11:26:28 +0200

    initial commit

--- JS/js-functions/js-functions_functions-with-parameters/js/index.js
+++ JS/js-functions/js-functions_functions-with-parameters/js/index.js
@@ -1,52 +1,25 @@
 console.clear();
 
-console.log("-------------");
-console.log("Exercise 1:");
-/*
-1: The function below is not executed. Can you figure out why? Find the bug and fix it.
-*/
+logSeparator("Exercise " + 1 + ":");
 
-// Uncomment the next line of code and figure out why it does not work:
-// logText;
-
+logText();
 function logText() {
   console.log("This text does not appear. Why?");
 }
 
-console.log("-------------");
-console.log("Exercise 2:");
-/*
-2: Look at the functions below. They are almost identical. Can you find a way to generalize them into a new function with an input parameter? Replace the function calls below with your new function.
-*/
+logSeparator("Exercise " + 2 + ":");
 
-function greetBob() {
-  console.log("Welcome Bob, good to see you again!");
+function greet(name) {
+  console.log("Welcome " + name + " good to see you again!");
 }
+greet("Bob");
+greet("Alice");
+greet("Mary");
 
-function greetAlice() {
-  console.log("Welcome Alice, good to see you again!");
+logSeparator("Exercise " + 3 + ":");
+
+function logSeparator(exerciseNum) {
+  console.log("-------------");
+  console.log(exerciseNum);
 }
-
-function greetMary() {
-  console.log("Welcome Mary, good to see you again!");
-}
-
-greetAlice();
-greetBob();
-greetMary();
-
-// --v-- write your code here --v--
-
-function greet() {}
-
-// --^-- write your code here --^--
-
-console.log("-------------");
-console.log("3:");
-/*
-3: We log a little separation text for each subtask. This is repetitive code, so lets write a function 'logSeparator' which takes the number of the exercise as an input parameter and logs the respective separator. then use this function to replace the existing separator logs in this javascript file.
-*/
-
-// --v-- write your code here --v--
-
-// --^-- write your code here --^--
+logSeparator("Exercise " + 4 + ":");
