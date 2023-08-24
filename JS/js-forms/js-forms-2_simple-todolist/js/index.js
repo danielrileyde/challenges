5d46ec291a88e460177f8c8a6f6a5276b7918f33
Daniel Riley <dann.riley@icloud.com>
2023-08-24 18:16:23 +0200

    create simple to do list with eventListener and function

--- JS/js-forms/js-forms-2_simple-todolist/js/index.js
+++ JS/js-forms/js-forms-2_simple-todolist/js/index.js
@@ -2,3 +2,31 @@ console.clear();
 
 const form = document.querySelector('[data-js="form"]');
 const todoList = document.querySelector('[data-js="todoList"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  createListItem(data);
+  console.log(data);
+
+  event.target.reset();
+  document.getElementById("headline").focus();
+});
+
+function createListItem(data) {
+  const listItem = document.createElement("li");
+  const headlineItem = document.createElement("h2");
+  const paragraphItem = document.createElement("p");
+
+  const headline = data.headline;
+  const task = data.task;
+
+  headlineItem.textContent = headline;
+  paragraphItem.textContent = task;
+
+  listItem.append(headlineItem);
+  listItem.append(paragraphItem);
+  todoList.append(listItem);
+}
