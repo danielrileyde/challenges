db75d252947ce51c98b0b77422b4ef2499fcf461
Daniel Riley <dann.riley@icloud.com>
2023-09-05 10:56:37 +0200

    wrote function arguments to pass tests

--- JS/js-unit-testing/js-unit-testing_get-animal/index.js
+++ JS/js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  if (animal === "cats") {
+    return "I totally love cats!";
+  }
+  if (animal == `${animal}`) {
+    return `I like ${animal}!`;
+  }
+  return "I do not like animals at all!";
+}
