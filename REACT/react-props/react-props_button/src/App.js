a950c0a8f0db87296ff8cdca1ccfd0e268ea6cb0
Daniel Riley <dann.riley@icloud.com>
2023-09-11 16:36:00 +0200

    created button component with props

--- REACT/react-props/react-props_button/src/App.js
+++ REACT/react-props/react-props_button/src/App.js
@@ -2,5 +2,26 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <>
+      <Button
+        backgroundColor="black"
+        color="#f9f9f9"
+        disabled={false}
+        text="Click me"
+      />
+      <Button color="#ff0000" disabled={true} text="Don't click" />
+    </>
+  );
+}
+
+function Button({ backgroundColor, color, disabled, text }) {
+  return (
+    <button
+      style={{ color: color, backgroundColor: backgroundColor }}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
 }
