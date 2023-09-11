141f08e40a15cfdbcd05b1142b3ff77e57c00dc6
Daniel Riley <dann.riley@icloud.com>
2023-09-11 15:55:58 +0200

    created sum component to sum value props

--- REACT/react-props/react-props_sum/src/App.js
+++ REACT/react-props/react-props_sum/src/App.js
@@ -2,5 +2,13 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum valueA={100} valueB={10} />;
+}
+
+function Sum({ valueA, valueB }) {
+  return (
+    <h1>
+      {valueA} + {valueB} = {valueA + valueB}
+    </h1>
+  );
 }
