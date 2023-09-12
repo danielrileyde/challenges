6c4d198f9216f90e63b8d8debc5e6eca96712545
Daniel Riley <dann.riley@icloud.com>
2023-09-12 11:30:51 +0200

    crete boxes component and use fragments

--- REACT/react-nesting/react-nesting_fragments/src/App.js
+++ REACT/react-nesting/react-nesting_fragments/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./styles.css";
 import Box from "./components/Box";
+import { Fragment } from "react";
 
 export default function App() {
   return (
     <main className="flex-container">
+      <Boxes />
+    </main>
+  );
+}
+
+function Boxes() {
+  return (
+    <Fragment>
       <Box color="#007bff" />
       <Box color="#fc3" />
       <Box color="#ff3333" />
-    </main>
+    </Fragment>
   );
 }
