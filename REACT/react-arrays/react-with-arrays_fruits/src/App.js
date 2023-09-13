6d990d80a2ed0e7ed258eb6749abdfe982892610
Daniel Riley <dann.riley@icloud.com>
2023-09-13 15:33:32 +0200

    create array for card component

--- REACT/react-arrays/react-with-arrays_fruits/src/App.js
+++ REACT/react-arrays/react-with-arrays_fruits/src/App.js
@@ -1,11 +1,45 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 0,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 1,
+      name: "🍓 Strawberry",
+      color: "Red",
+    },
+    {
+      id: 2,
+      name: "🥥 Coconut",
+      color: "Brown",
+    },
+    {
+      id: 3,
+      name: "🫐 Blueberry",
+      color: "Blue",
+    },
+    {
+      id: 4,
+      name: "🥝 Kiwi",
+      color: "green",
+    },
+  ];
 
   return (
-    <div className="app">
-      <Card name="🍌 banana" />
-    </div>
+    <ul>
+      {fruits.map(({ id, name, color }) => (
+        <Card key={id} name={name} color={color} />
+      ))}
+    </ul>
   );
+
+  // return (
+  //   <div className="app">
+  //     <Card name="🍌 banana" />
+  //   </div>
+  // );
 }
