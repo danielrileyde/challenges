49ea3767d52b35c6481ef33b83ca75fcb095eaa8
Daniel Riley <dann.riley@icloud.com>
2023-09-14 13:08:16 +0200

    refactor the handleSubmit

--- REACT/react-state-two/react-state-2_favourite-holiday/src/App.js
+++ REACT/react-state-two/react-state-2_favourite-holiday/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [searchHoliday, setSearchHoliday] = useState("");
+  const [holidayInput, setHolidayInput] = useState("");
+  const [searchDate, setSearchDate] = useState("");
+  const [dateInput, setDateInput] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
+    setSearchHoliday(holidayInput);
+    setSearchDate(dateInput);
+    setDateInput("");
+    setHolidayInput("");
+    event.target.elements.holiday.focus();
   }
 
   return (
@@ -21,19 +32,27 @@ export default function App() {
           type="text"
           name="holiday"
           placeholder="e.g. Christmas"
+          value={holidayInput}
+          onChange={(event) => setHolidayInput(event.target.value)}
         />
         <label htmlFor="date">Date: </label>
-        <input id="date" type="date" name="date" />
+        <input
+          id="date"
+          type="date"
+          name="date"
+          value={dateInput}
+          onChange={(event) => setDateInput(event.target.value)}
+        />
         <button type="submit" className="form__submit-button">
           Submit
         </button>
       </form>
       <h2>Output of Submitted Data</h2>
       <p>
-        Favourite Holiday: <span className="output">New Year</span>
+        Favourite Holiday: <span className="output">{searchHoliday}</span>
       </p>
       <p>
-        Date: <span className="output">Well...</span>
+        Date: <span className="output">{searchDate}</span>
       </p>
     </div>
   );
