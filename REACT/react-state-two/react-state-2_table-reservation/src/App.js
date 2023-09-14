125e0d6906570a07be7d604bb73b7628501695b2
Daniel Riley <dann.riley@icloud.com>
2023-09-14 12:02:39 +0200

    lift people state up to app and dynamically input people

--- REACT/react-state-two/react-state-2_table-reservation/src/App.js
+++ REACT/react-state-two/react-state-2_table-reservation/src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  function handlePeopleIncrement() {
+    setPeople(people + 1);
+  }
+  function handlePeopleDecrement() {
+    //prevent minus reservation for decrement past 0.
+    setPeople(Math.max(0, people - 1));
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter
+        onPeopleIncrement={handlePeopleIncrement}
+        onPeopleDecrement={handlePeopleDecrement}
+      />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
