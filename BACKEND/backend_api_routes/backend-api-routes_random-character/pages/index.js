3f453ff24df5017dbefc4677a8d59a96b4e5125d
Daniel Riley <dann.riley@icloud.com>
2023-10-09 17:35:51 +0200

    create random charcater api route

--- BACKEND/backend_api_routes/backend-api-routes_random-character/pages/index.js
+++ BACKEND/backend_api_routes/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,27 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, isLoading } = useSWR("/api/random-character", fetcher);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  const { firstName, lastName, twitterName, geohash } = data;
+
+  return (
+    <>
+      <h1>Random Character</h1>
+      <p>{firstName}</p>
+      <p>{lastName}</p>
+      <p>{twitterName}</p>
+      <p>{geohash}</p>
+    </>
+  );
 }
